834e41a355dcc1fa60ba077b69a5a5320ab3590c
abestroka <abestroka@gmail.com>
2024-03-13 19:59:47 -0500

    bash for 1000 image sets

--- run_cell_seg.sh
+++ run_cell_seg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 #PBS -l select=1:system=polaris
 #PBS -l place=scatter
-#PBS -l walltime=1:00:00
-#PBS -q debug 
+#PBS -l walltime=12:00:00
+#PBS -q prod 
 #PBS -A APSDataAnalysis
 #PBS -l filesystems=home:eagle
 
@@ -30,30 +30,30 @@ export no_proxy="admin,polaris-adminvm-01,localhost,*.cm.polaris.alcf.anl.gov,po
 
 echo "NUM_OF_NODES= ${NNODES} TOTAL_NUM_RANKS= ${NTOTRANKS} RANKS_PER_NODE= ${NRANKS_PER_NODE} THREADS_PER_RANK= ${NTHREADS}"
 SECONDS=0
-echo "pulling metadata"
-# pull metadata, save to csv (TODO pare down into just filenames and targets, then export as lists)
-python ~/workspace/JUMP_vision_model/pull_meta.py -s 10
-echo "metadata pulled at $SECONDS seconds"
-image_set=1
-echo "pulling images from aws"
+# echo "pulling metadata"
+# # pull metadata, save to csv (TODO pare down into just filenames and targets, then export as lists)
+# python ~/workspace/JUMP_vision_model/pull_meta.py -s 10
+# echo "metadata pulled at $SECONDS seconds"
+# image_set=1
+# echo "pulling images from aws"
 # python ~/workspace/JUMP_vision_model/pull_images.py -s "$image_set"
-# for i in {1..1000}
-# do
-#     # echo "image set"
-#     # echo "$i"
-#     python ~/workspace/JUMP_vision_model/pull_images.py --index $i
-#     target=$(head -n 1 '/home/astroka/workspace/JUMP_vision_model/target_name.txt')
+for i in {1..1000}
+do
+    # echo "image set"
+    # echo "$i"
+    python ~/workspace/JUMP_vision_model/pull_images.py --index $i
+    target=$(head -n 1 '/home/astroka/workspace/JUMP_vision_model/target_name.txt')
 
-#     # cellprofiler into target directory
-#     # singularity run cellprofiler_4.2.6.sif -c -r -p ~/workspace/JUMP_vision_model/my_project_421.cppipe -i /eagle/projects/APSDataAnalysis/LUCID/image_temp -o /eagle/projects/APSDataAnalysis/LUCID/segmented_images/"$i"/
-#     singularity run cellprofiler_4.2.6.sif -c -r -p ~/workspace/JUMP_vision_model/my_project_421.cppipe -i ~/workspace/JUMP_vision_model/image_temp -o ~/workspace/results/segmented_image_temp/"$target"/
+    # cellprofiler into target directory
+    # singularity run cellprofiler_4.2.6.sif -c -r -p ~/workspace/JUMP_vision_model/my_project_421.cppipe -i /eagle/projects/APSDataAnalysis/LUCID/image_temp -o /eagle/projects/APSDataAnalysis/LUCID/segmented_images/"$i"/
+    singularity run cellprofiler_4.2.6.sif -c -r -p ~/workspace/JUMP_vision_model/my_project_421.cppipe -i ~/workspace/JUMP_vision_model/image_temp -o ~/workspace/results/segmented_image_temp/"$target"/
 
-#     #iterate through target directory and change names of cells
-#     # check if target directory exists on eagle, if not create one, and transfer contents
-#     # delete local directory
-#     python ~/workspace/JUMP_vision_model/change_names.py --target "$target"
+    #iterate through target directory and change names of cells
+    # check if target directory exists on eagle, if not create one, and transfer contents
+    # delete local directory
+    python ~/workspace/JUMP_vision_model/change_names.py --target "$target"
 
-#     echo "image set $i segmented in $SECONDS seconds"
+    echo "image set $i segmented in $SECONDS seconds"
 
 
 # done
