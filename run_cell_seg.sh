53be961b674427b468f8127e3543ada997ef91cd
abestroka <abestroka@gmail.com>
2024-03-22 10:30:19 -0500

    updated path

--- run_cell_seg.sh
+++ run_cell_seg.sh
@@ -8,7 +8,8 @@
 
 module load singularity
 module load conda
-conda activate cellprofiler
+# conda activate cellprofiler
+conda activate cellseg
 cd ~/workspace
 
 # MPI example w/ 4 MPI ranks per node w/ threads spread evenly across cores (1 thread per core)
@@ -34,33 +35,33 @@ SECONDS=0
 # # pull metadata, save to csv (TODO pare down into just filenames and targets, then export as lists)
 # python ~/workspace/JUMP_vision_model/pull_meta.py
 # echo "metadata pulled at $SECONDS seconds"
-image_set=1
-echo "pulling images from aws"
-for i in {1..1000}
-do
-    # echo "image set"
-    # echo "$i"
-    python ~/workspace/JUMP_vision_model/pull_images.py --index $i
-    target=$(head -n 1 '/home/astroka/workspace/JUMP_vision_model/target_name.txt')
+# image_set=1
+# echo "pulling images from aws"
+# for i in {1..1000}
+# do
+#     # echo "image set"
+#     # echo "$i"
+#     python ~/workspace/JUMP_vision_model/pull_images.py --index $i
+#     target=$(head -n 1 '/home/astroka/workspace/JUMP_vision_model/target_name.txt')
 
-    # cellprofiler into target directory
-    # singularity run cellprofiler_4.2.6.sif -c -r -p ~/workspace/JUMP_vision_model/my_project_421.cppipe -i /eagle/projects/FoundEpidem/astroka/image_temp -o /eagle/projects/APSDataAnalysis/LUCID/segmented_images/"$i"/
-    singularity run cellprofiler_4.2.6.sif -c -r -p ~/workspace/JUMP_vision_model/my_project_421.cppipe -i ~/workspace/JUMP_vision_model/image_temp -o ~/workspace/results/segmented_image_temp/"$target"/
+#     # cellprofiler into target directory
+#     # singularity run cellprofiler_4.2.6.sif -c -r -p ~/workspace/JUMP_vision_model/my_project_421.cppipe -i /eagle/projects/FoundEpidem/astroka/image_temp -o /eagle/projects/APSDataAnalysis/LUCID/segmented_images/"$i"/
+#     singularity run cellprofiler_4.2.6.sif -c -r -p ~/workspace/JUMP_vision_model/my_project_421.cppipe -i ~/workspace/JUMP_vision_model/image_temp -o ~/workspace/results/segmented_image_temp/"$target"/
 
-    #iterate through target directory and change names of cells
-    # check if target directory exists on eagle, if not create one, and transfer contents
-    # delete local directory
-    python ~/workspace/JUMP_vision_model/change_names.py --target "$target"
+#     #iterate through target directory and change names of cells
+#     # check if target directory exists on eagle, if not create one, and transfer contents
+#     # delete local directory
+#     python ~/workspace/JUMP_vision_model/change_names.py --target "$target"
 
-    echo "image set $i segmented in $SECONDS seconds"
+#     echo "image set $i segmented in $SECONDS seconds"
 
 
-done
+# done
 
-# model
+model
 
-# echo "time before running model: $SECONDS seconds"
+echo "time before running model: $SECONDS seconds"
 
-# echo "running vision transformer"
-# python ~/workspace/JUMP_vision_model/ViT.py
-# echo "model finished at $SECONDS seconds"
+echo "running vision transformer"
+python ~/workspace/JUMP_vision_model/ViT.py
+echo "model finished at $SECONDS seconds"
