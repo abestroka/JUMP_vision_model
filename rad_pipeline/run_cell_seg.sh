693d956d73f532f4c9056f7eff105f3cb6dc10ee
abestroka <abestroka@gmail.com>
2024-07-09 14:42:03 -0500

    path fix

--- rad_pipeline/run_cell_seg.sh
+++ rad_pipeline/run_cell_seg.sh
@@ -39,14 +39,15 @@ SECONDS=0
 
 # concatenate all images into a single excel file, and get length, then iterate through
 images="/eagle/FoundEpidem/astroka/pilot_imgs/Test1/20240517_OSU_HTSC_MW_ANL_CellPainting_P3_2__2024-05-17T15_59_05-Measurement1/Images"
+image_dir_path="/eagle/projects/FoundEpidem/astroka/pilot_imgs/Test1/20240517_OSU_HTSC_MW_ANL_CellPainting_P3_2__2024-05-17T15_59_05-Measurement1/all_images.xlsx"
 # plate="Plate3"
 # python ~/workspace/JUMP_vision_model/rad_pipeline/concat_images.py --image_path $images --plate $plate
-num=$(head -n 1 '/home/astroka/workspace/JUMP_vision_model/rad/pipeline/num_images.txt')
+# num=$(head -n 1 '/home/astroka/workspace/JUMP_vision_model/rad/pipeline/num_images.txt')
 #TODO: extra function for extracting desired samples ie 1 of each well
 for i in {1..2}
 do
     # echo "get next image set, and id target name from excel file"
-    python ~/workspace/JUMP_vision_model/rad_pipeline/pull_images.py --index $i --path images
+    python ~/workspace/JUMP_vision_model/rad_pipeline/pull_images.py --index $i --path image_dir_path
 
     # target=$(head -n 1 '/home/astroka/workspace/JUMP_vision_model/target_name.txt')
 
