8b8854c61ceafb7f6f33f0173df214cfc22eee39
abestroka <abestroka@gmail.com>
2024-10-15 14:22:45 -0500

    more robust graphs

--- rad_pipeline/generate_histograms.py
+++ rad_pipeline/generate_histograms.py
@@ -8,35 +8,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
 def generate_graph(src_dir):
     all_sum = None
     num_files = 0 
-    images = os.listdir(src_dir)
+    # images = os.listdir(src_dir)
     to_remove = ['ImageNumber', 'ObjectNumber']
+    average_df = pd.DataFrame()
 
-    for image in images:
-        if "data" in image: #new data found
-            curr_path = os.path.join(src_dir, image)
-            df = pd.read_csv(curr_path)
-            df = df.drop(columns=to_remove, errors='ignore')
-            row = df.iloc[0]
-            
+    treatments = ['untreated', 'Compound_1', 'Compound_2', 'Compound_3', 'Compound_4', 'Compound_5', 'Compound_6', 'Compound_7', 'Compound_8', 'Compound_9', 'Compound_10', 'Compound_11', 'Compound_12', 'Compound_13', 'Compound_14', 'Compound_15', 'Compound_16', 'Compound_17']
+    for treatment in treatments:
+        base_dir = src_dir + treatment + '/'
+        for image in images:
+            if "data" in image: #new data found
+                curr_path = os.path.join(base_dir, image)
+                df = pd.read_csv(curr_path)
+                df = df.drop(columns=to_remove, errors='ignore')
+                row = df.iloc[0]
+                
 
-            if all_sum is None:
-                all_sum = row
-            else:
-                all_sum += row
+                if all_sum is None:
+                    all_sum = row
+                else:
+                    all_sum += row
+                
+                num_files += 1
             
-            num_files += 1
-        
-        if num_files == 10:
-            break
+            if num_files == 10:
+                break
     
-    average_row = all_sum/num_files
-    average_df = pd.DataFrame([average_row])
-    print("FINAL")
-    print(average_df)
+            average_row = all_sum/num_files
+            # average_df = pd.DataFrame([average_row])
+            average_df.append(average_row, ignore_index=True)
+            print("FINAL")
+            print(average_df)
 
     # Generate histogram from the values in average_df
     # average_values = average_df.iloc[0].values  # Get the values from the DataFrame
