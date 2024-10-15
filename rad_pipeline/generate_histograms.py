bb9d7c7abdfd31d8bb1e0e504e96595777ab08b2
abestroka <abestroka@gmail.com>
2024-10-15 16:13:27 -0500

    Update generate_histograms.py

--- rad_pipeline/generate_histograms.py
+++ rad_pipeline/generate_histograms.py
@@ -13,7 +13,7 @@ def generate_graph(src_dir):
     num_files = 0 
     # images = os.listdir(src_dir)
     to_remove = ['ImageNumber', 'ObjectNumber']
-    average_df = None
+    average_df = pd.DataFrame()
 
     treatments = ['untreated', 'Compound_1', 'Compound_2', 'Compound_3', 'Compound_4', 'Compound_5', 'Compound_6', 'Compound_7', 'Compound_8', 'Compound_9', 'Compound_10', 'Compound_11', 'Compound_12', 'Compound_13', 'Compound_14', 'Compound_15', 'Compound_16', 'Compound_17']
     for treatment in treatments:
@@ -22,6 +22,7 @@ def generate_graph(src_dir):
         base_dir = src_dir + treatment + '/'
         print("BASE DIR", base_dir)
         images = os.listdir(base_dir)
+
         for image in images:
             if "data" in image: #new data found
                 curr_path = os.path.join(base_dir, image)
@@ -42,36 +43,31 @@ def generate_graph(src_dir):
             
             if num_files == 10:
                 break
+
         print("ALL SUM 2", all_sum)
         print(num_files)
         average_row = all_sum/num_files
         print("AVG ROW", average_row)
         print("AVG DF", average_df)
+        average_row['Treatment'] = treatment
+        average_row_df = pd.DataFrame([average_row])
         # average_df = pd.DataFrame([average_row])
+        average_df = pd.concat([average_df, average_row_df], ignore_index=True)
         if average_df is None:
             average_df = average_row
         else:
             average_df = pd.concat([average_df, average_row], ignore_index=False)
         print("FINAL")
         print(average_df)
+        
+    average_df.set_index('Treatment', inplace=True)
 
-    # Generate histogram from the values in average_df
-    # average_values = average_df.iloc[0].values  # Get the values from the DataFrame
-
-    # plt.hist(average_values, bins=10, edgecolor='black')
-    # plt.title('Histogram of Averages')
-    # plt.xlabel('Values')
-    # plt.ylabel('Frequency')
-
-    # Generate bar chart from the values in average_df
-    columns = average_df.columns  # Get column names
-    values = average_df.iloc[0].values  # Get the values from the first row
-
-    plt.bar(columns, values)  # Create a bar chart with columns on the x-axis and values on the y-axis
-    plt.title('Bar Chart of Averages')
-    plt.xlabel('Columns')
-    plt.ylabel('Values')
-    plt.xticks(rotation=45, ha='right')  # Rotate column labels for better readability
+    # Plotting the bar chart for all treatments' average values
+    average_df.T.plot(kind='bar', figsize=(15, 8))  # Transpose to have features on x-axis
+    plt.title('Bar Chart of Averages for Each Treatment')
+    plt.xlabel('Features')
+    plt.ylabel('Average Values')
+    plt.xticks(rotation=45, ha='right') # Rotate column labels for better readability
 
 
     # Save the histogram to a file
