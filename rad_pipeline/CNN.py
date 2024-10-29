e2eddf0b8be099185601959aa13e7d31ad1974a1
abestroka <abestroka@gmail.com>
2024-10-29 11:45:07 -0500

    larger 4 week compound cnn run test

--- rad_pipeline/CNN.py
+++ rad_pipeline/CNN.py
@@ -3,53 +3,105 @@ import torch.nn as nn
 from torchvision import datasets, transforms, models
 from torch.utils.data import DataLoader
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def cnn(tiles, color, percent, batch):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),  # ResNet input size
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # ImageNet mean and std
-])
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),  # ResNet input size
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # ImageNet mean and std
+    ])
 
-test_dataset = datasets.ImageFolder(root='/eagle/FoundEpidem/astroka/fib_and_htert/cnn_data/week_three/fib_control', transform=transform)
-# test_dataset = datasets.ImageFolder(root='/eagle/FoundEpidem/astroka/fib_and_htert/week_one/results/fib_control', transform=transform)
-test_loader = DataLoader(test_dataset, batch_size=8, shuffle=False)
+    test_dataset = datasets.ImageFolder(root='/eagle/FoundEpidem/astroka/fib_and_htert/cnn_data/week_three/fib_control', transform=transform)
+    # test_dataset = datasets.ImageFolder(root='/eagle/FoundEpidem/astroka/fib_and_htert/week_one/results/fib_control', transform=transform)
+    test_loader = DataLoader(test_dataset, batch_size=batch, shuffle=False)
 
-print("CLASSES")
-print(test_dataset.class_to_idx)
+    print("CLASSES")
+    print(test_dataset.class_to_idx)
 
-print("")
+    print("")
 
-num_classes = len(test_dataset.classes)
-print(f"Number of classes: {num_classes}")
-print(f"Class-to-index mapping: {test_dataset.class_to_idx}")
-
-
-model = models.resnet50(pretrained=True)
-
-# num_classes = 5
-
-model.fc = nn.Linear(model.fc.in_features, num_classes)
-
-model = model.to(device)
-model.eval()  
+    num_classes = len(test_dataset.classes)
+    print(f"Number of classes: {num_classes}")
+    print(f"Class-to-index mapping: {test_dataset.class_to_idx}")
 
 
+    model = models.resnet50(pretrained=True)
 
-correct = 0
-total = 0
+    # num_classes = 5
 
-with torch.no_grad():
-    for inputs, labels in test_loader:
-        inputs, labels = inputs.to(device), labels.to(device)
+    model.fc = nn.Linear(model.fc.in_features, num_classes)
 
-        outputs = model(inputs)
-
-        _, predicted = torch.max(outputs, 1)
-
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+    model = model.to(device)
+    model.eval()  
 
 
-accuracy = 100 * correct / total
-print(f'Accuracy of the pre-trained model on the test dataset: {accuracy:.2f}%')
+
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            outputs = model(inputs)
+
+            _, predicted = torch.max(outputs, 1)
+
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+
+    accuracy = 100 * correct / total
+    print(f'TILES NUMBER: {tiles:.2f}%', f'COLOR: {color:.2f}%', f'THRESHOLD: {percent:.2f}%', f'BATCH: {batch:.2f}%')
+    print(f'Accuracy of the pre-trained model on the test dataset: {accuracy:.2f}%')
+
+
+def main(args):
+    tiles = vars(args)["tiles"]
+    color = vars(args)["color"]
+    percent = vars(args)["percent"]
+    batch_num = vars(args)["batch"]
+
+    batch = 2**int(batch_num)
+
+    cnn(tiles, color, percent, batch)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+    "-c",
+    "--color",
+    help="source folder name",
+    type=str,
+    required=True,
+    )
+
+    parser.add_argument(
+    "-t",
+    "--tiles",
+    help="source folder name",
+    type=str,
+    required=True,
+    )
+
+    parser.add_argument(
+    "-p",
+    "--percent",
+    help="source folder name",
+    type=str,
+    required=True,
+    )
+
+    parser.add_argument(
+    "-b",
+    "--batch",
+    help="source folder name",
+    type=str,
+    required=True,
+    )
+
+
+    args = parser.parse_args()
+    main(args)
--- rad_pipeline/tile_images.py
+++ rad_pipeline/tile_images.py
@@ -1,13 +1,14 @@
 from PIL import Image
 import os
 
-def is_tile_yellow(tile, threshold, percentage_required):
+def is_tile_yellow(tile, threshold, percentage_required, curr_color):
     tile = tile.convert('RGB')
     yellow_pixels = 0
     total_pixels = tile.width * tile.height
-
-    yellow_rgb = (255, 255, 0)
-    # yellow_rgb = (255, 255, 255)
+    if curr_color == "yellow":
+        yellow_rgb = (255, 255, 0)
+    elif curr_color == "white":
+        yellow_rgb = (255, 255, 255)
     tolerance = 50 
 
     for y in range(tile.height):
@@ -30,7 +31,7 @@ def threshold_images(image_path, confluency):
             with open(curr_path, 'r') as f:
                 val = f.read()
                 print("VAL", val)
-            if float(val) >= confluency and float(val) <= 0.75:
+            if float(val) >= confluency and float(val):
                 print("PASS")
                 img = str(file[:12]) + '.png'
                 print('IMG', img)
@@ -40,10 +41,10 @@ def threshold_images(image_path, confluency):
 
             
 
-def segment_image(image_path, output_folder, rows, cols, yellow_threshold, yellow_percentage):
+def segment_image(image_path, output_folder, rows, cols, yellow_threshold, yellow_percentage, curr_color):
 
     # images = os.listdir(image_path)
-    confluency = 0.25
+    confluency = 0.2
     images = threshold_images(image_path, confluency)
     tile_num = 0
     for image in images:
@@ -71,7 +72,7 @@ def segment_image(image_path, output_folder, rows, cols, yellow_threshold, yello
                     # export tile
                     # tile.save(os.path.join(output_folder, f'tile_{tile_num}.png'))
                     # tile_num += 1
-                    if is_tile_yellow(tile, yellow_threshold, yellow_percentage):
+                    if is_tile_yellow(tile, yellow_threshold, yellow_percentage, curr_color):
                         tile.save(os.path.join(output_folder, f'tile_{tile_num}.png'))
                         tile_num += 1
 
@@ -85,27 +86,71 @@ def segment_image(image_path, output_folder, rows, cols, yellow_threshold, yello
 # output_folder = f'/eagle/FoundEpidem/astroka/tiles/'  # Folder to save the tiles
 
 # Set rows and cols for segmentation
-rows, cols = 10, 10
+def main(args):
+    tiles = vars(args)["tiles"]
+    color = vars(args)["color"]
+    percent = vars(args)["percent"]
+    # rows, cols = 10, 10
+    rows, cols = int(tiles), int(tiles)
 
-yellow_threshold = 50         # Tolerance for yellow (e.g. ±50 from 255,255,0)
-yellow_percentage = 1.0       # Minimum percentage of yellow pixels required to save the tile
 
-# rads = ['0.001', '0.01', '0.1', '1.0', '2.0']
-rads = ['untreated', 'Compound_1', 'Compound_2', 'Compound_3', 'Compound_4', 'Compound_5', 'Compound_6', 'Compound_7', 'Compound_8', 'Compound_9', 'Compound_10', 'Compound_11', 'Compound_12', 'Compound_13', 'Compound_14', 'Compound_15', 'Compound_16', 'Compound_17']
-weeks = ['week_one', 'week_two', 'week_three']
+    yellow_threshold = 50         # Tolerance for yellow (e.g. ±50 from 255,255,0)
+    # yellow_percentage = 1.0       # Minimum percentage of yellow pixels required to save the tile
+    yellow_percentage = 0.1 * float(percent)
+    if color == "1":
+        curr_color = "white"
+    else:
+        curr_color = "yellow"
+    # rads = ['0.001', '0.01', '0.1', '1.0', '2.0']
+    rads = ['untreated', 'Compound_1', 'Compound_2', 'Compound_3', 'Compound_4', 'Compound_5', 'Compound_6', 'Compound_7', 'Compound_8', 'Compound_9', 'Compound_10', 'Compound_11', 'Compound_12', 'Compound_13', 'Compound_14', 'Compound_15', 'Compound_16', 'Compound_17']
+    weeks = ['week_one', 'week_two', 'week_three', 'week_four']
 
-for week in weeks:
-    for rad in rads:
-        # image_path = '/eagle/FoundEpidem/astroka/fib_and_htert/'+week+'/results/fib_rad/'+rad+'/'
-        # output_folder = f'/eagle/FoundEpidem/astroka/fib_and_htert/cnn_data/'+week+'/fib_rad/'+rad+'/'
-        image_path = '/eagle/FoundEpidem/astroka/fib_and_htert/'+week+'/results/fib_control/'+rad+'/'
-        output_folder = f'/eagle/FoundEpidem/astroka/fib_and_htert/cnn_data/'+week+'/fib_control/'+rad+'/'
-        # if os.path.isdir(output_folder) == False:
-        #     os.mkdir(output_folder)
-            
-        segment_image(image_path, output_folder, rows, cols, yellow_threshold, yellow_percentage)
+    for week in weeks:
+        for rad in rads:
+            # image_path = '/eagle/FoundEpidem/astroka/fib_and_htert/'+week+'/results/fib_rad/'+rad+'/'
+            # output_folder = f'/eagle/FoundEpidem/astroka/fib_and_htert/cnn_data/'+week+'/fib_rad/'+rad+'/'
+            image_path = '/eagle/FoundEpidem/astroka/fib_and_htert/'+week+'/results/fib_control/'+rad+'/'
+            output_folder = f'/eagle/FoundEpidem/astroka/fib_and_htert/cnn_data/'+week+'/fib_control/'+rad+'/'
+            # if os.path.isdir(output_folder) == False:
+            #     os.mkdir(output_folder)
+                
+            segment_image(image_path, output_folder, rows, cols, yellow_threshold, yellow_percentage, curr_color)
 
-        
+            #if contents of output folder are empty, delete output folder
+
+            if os.path.isdir(output_folder) and not os.listdir(output_folder):
+                os.rmdir(output_folder)  
 
 # Segment the image
 # segment_image(image_path, output_folder, rows, cols, yellow_threshold, yellow_percentage)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+    "-c",
+    "--color",
+    help="source folder name",
+    type=str,
+    required=True,
+    )
+
+    parser.add_argument(
+    "-t",
+    "--tiles",
+    help="source folder name",
+    type=str,
+    required=True,
+    )
+
+    parser.add_argument(
+    "-p",
+    "--percent",
+    help="source folder name",
+    type=str,
+    required=True,
+    )
+
+
+    args = parser.parse_args()
+    main(args)
