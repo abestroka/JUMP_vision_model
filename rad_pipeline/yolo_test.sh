f1a55f1dd30a437a88c0bfbeace5b2c060e2f106
abestroka <abestroka@gmail.com>
2024-12-04 12:09:04 -0600

    update yolo

--- rad_pipeline/yolo_test.sh
+++ rad_pipeline/yolo_test.sh
@@ -7,13 +7,14 @@
 #PBS -l filesystems=home:eagle
 
 # module load singularity
-module use /soft/spack/gcc/0.6.1/install/modulefiles/Core
-module load apptainer
+# module use /soft/spack/gcc/0.6.1/install/modulefiles/Core
+# module load apptainer
 
-# module use /soft/modulefiles 
-# module load conda
+module use /soft/modulefiles 
+module load conda
 # conda activate cellprofiler
 # conda activate cellseg
+conda activate yolo
 cd ~/workspace
 
 # MPI example w/ 4 MPI ranks per node w/ threads spread evenly across cores (1 thread per core)
