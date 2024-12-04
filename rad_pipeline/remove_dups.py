bb9e955c713f7b9d69c961e375611b7fd58ffbfb
abestroka <abestroka@gmail.com>
2024-12-04 15:40:33 -0600

    Update remove_dups.py

--- rad_pipeline/remove_dups.py
+++ rad_pipeline/remove_dups.py
@@ -6,10 +6,10 @@ path = '/eagle/FoundEpidem/astroka/yolo/rpe_rad_2'
 for root, dirs, files in os.walk(path):
     print(f"Current Directory: {root}")
     
-    # List of directories in the current directory
-    for dir_name in dirs:
-        print(f"  Directory: {os.path.join(root, dir_name)}")
-    
-    # List of files in the current directory
+    # # List of directories in the current directory
+    # for dir_name in dirs:
+    #     print(f"  Directory: {os.path.join(root, dir_name)}")
+
     for file_name in files:
-        print(f"  File: {os.path.join(root, file_name)}")
+        # print(f"  File: {os.path.join(root, file_name)}")
+        print(file_name)
