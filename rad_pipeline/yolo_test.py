052ab6fcd3c1390e869bc38b3ddba5c40e8bf303
abestroka <abestroka@gmail.com>
2024-12-09 09:15:18 -0600

    Update yolo_test.py

--- rad_pipeline/yolo_test.py
+++ rad_pipeline/yolo_test.py
@@ -37,8 +37,7 @@ import random
 
 
 
-files = glob.glob('/homes/ngetty/data/LUCID/fib_and_htert_results/**/fib_control/**/*.png')
-files = glob.glob('/eagle/FoundEpidem/astroka/yolo/rpe_rad_seg_1/images/**/*.png')
+files = glob.glob('/eagle/FoundEpidem/astroka/yolo/rpe_rad_seg_2/images/**/*.png')
 print("NUM FILES")
 print(len(files))
 print(" ")
@@ -177,13 +176,13 @@ labels = [x.split('/')[-2] for x in files]
 train_dir = prepare_classification_data(
         np.array(files)[train_idx],
         np.array(labels)[train_idx],
-        '/eagle/FoundEpidem/astroka/yolo/rpe_rad_seg_1/data/dataset/train'
+        '/eagle/FoundEpidem/astroka/yolo/rpe_rad_seg_2/data/dataset/train'
     )
     
 val_dir = prepare_classification_data(
     np.array(files)[test_idx],
     np.array(labels)[test_idx],
-    '/eagle/FoundEpidem/astroka/yolo/rpe_rad_seg_1/data/dataset/val'
+    '/eagle/FoundEpidem/astroka/yolo/rpe_rad_seg_2/data/dataset/val'
 )
 
 # Define your class names
@@ -194,7 +193,7 @@ val_dir = prepare_classification_data(
 #        'Compound_9', 'untreated']
 
 # class_names = ['0.001', '0.01', '0.1', '1.0', '2.0']
-class_names = ['0.001', '0.1', '1.0', '2.0']
+class_names = ['0.001', '0.01', '0.1', '1.0', '2.0']
 
 
 
@@ -203,7 +202,7 @@ setup_classification_config(
     train_dir=train_dir,
     val_dir=val_dir,
     class_names=class_names,
-    config_path = '/eagle/FoundEpidem/astroka/yolo/rpe_rad_seg_1/data/datasets/config.yaml'
+    config_path = '/eagle/FoundEpidem/astroka/yolo/rpe_rad_seg_2/data/datasets/config.yaml'
     )
 
 # g = glob.glob('/eagle/FoundEpidem/astroka/yolo/dataset/**/**/*.png')
