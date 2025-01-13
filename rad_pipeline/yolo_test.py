acd4253d18d5b3366a3725d6009f538be5d24e3f
abestroka <abestroka@gmail.com>
2025-01-13 12:08:22 -0600

    yolo for tiled images

--- rad_pipeline/yolo_test.py
+++ rad_pipeline/yolo_test.py
@@ -14,26 +14,26 @@ import os
 import random
 
 # Path to the images directory
-# base_path = '/eagle/FoundEpidem/astroka/yolo/fib_control_seg_4/images'
+base_path = '/eagle/FoundEpidem/astroka/yolo/fib_control_seg_4/images'
 
-# # Number of files to keep in each subdirectory
-# num_files_to_keep = 250
+# Number of files to keep in each subdirectory
+num_files_to_keep = 200
 
-# # Find all subdirectories under the images directory
-# subdirs = [d for d in glob.glob(os.path.join(base_path, '*')) if os.path.isdir(d)]
+# Find all subdirectories under the images directory
+subdirs = [d for d in glob.glob(os.path.join(base_path, '*')) if os.path.isdir(d)]
 
-# for subdir in subdirs:
-#     # Find all PNG files in the current subdirectory
-#     files = glob.glob(os.path.join(subdir, '*.png'))
+for subdir in subdirs:
+    # Find all PNG files in the current subdirectory
+    files = glob.glob(os.path.join(subdir, '*.png'))
     
-#     # If the number of files exceeds the limit, delete extras
-#     if len(files) > num_files_to_keep:
-#         files_to_delete = random.sample(files, len(files) - num_files_to_keep)
-#         for file in files_to_delete:
-#             os.remove(file)
-#         print(f"Deleted {len(files_to_delete)} files from {subdir}")
-#     else:
-#         print(f"{subdir} has {len(files)} files, no files deleted.")
+    # If the number of files exceeds the limit, delete extras
+    if len(files) > num_files_to_keep:
+        files_to_delete = random.sample(files, len(files) - num_files_to_keep)
+        for file in files_to_delete:
+            os.remove(file)
+        print(f"Deleted {len(files_to_delete)} files from {subdir}")
+    else:
+        print(f"{subdir} has {len(files)} files, no files deleted.")
 
 ###############
 ###########
@@ -211,6 +211,8 @@ import random
 
 # print("G", g)
 
+#################
+#############
 
 # Load a COCO-pretrained YOLO11n model
 model = YOLO("yolo11x-cls.pt")
@@ -222,37 +224,3 @@ results = model.train(data="/eagle/FoundEpidem/astroka/yolo/fib_control_seg_4/da
 
 
 
-
-
-
-
-
-# engine/trainer: task=classify, mode=train, model=yolo11x-cls.pt, data=./datasets/, epochs=100, time=None, patience=100, batch=16, imgsz=640, save=True, save_period=-1, cache=False, device=None, workers=8, project=None, name=train11, exist_ok=False, pretrained=True, optimizer=auto, verbose=True, seed=0, deterministic=True, single_cls=False, rect=False, cos_lr=False, close_mosaic=10, resume=False, amp=True, fraction=1.0, profile=False, freeze=None, multi_scale=False, overlap_mask=True, mask_ratio=4, dropout=0.0, val=True, split=val, save_json=False, save_hybrid=False, conf=None, iou=0.7, max_det=300, half=False, dnn=False, plots=True, source=None, vid_stride=1, stream_buffer=False, visualize=False, augment=False, agnostic_nms=False, classes=None, retina_masks=False, embed=None, show=False, save_frames=False, save_txt=False, save_conf=False, save_crop=False, show_labels=True, show_conf=True, show_boxes=True, line_width=None, format=torchscript, keras=False, optimize=False, int8=False, dynamic=False, simplify=True, opset=None, workspace=4, nms=False, lr0=0.01, lrf=0.01, momentum=0.937, weight_decay=0.0005, warmup_epochs=3.0, warmup_momentum=0.8, warmup_bias_lr=0.1, box=7.5, cls=0.5, dfl=1.5, pose=12.0, kobj=1.0, label_smoothing=0.0, nbs=64, hsv_h=0.015, hsv_s=0.7, hsv_v=0.4, degrees=0.0, translate=0.1, scale=0.5, shear=0.0, perspective=0.0, flipud=0.0, fliplr=0.5, bgr=0.0, mosaic=1.0, mixup=0.0, copy_paste=0.0, copy_paste_mode=flip, auto_augment=randaugment, erasing=0.4, crop_fraction=1.0, cfg=None, tracker=botsort.yaml, save_dir=/home/astroka/yolo/runs/classify/train11
-
-
-# [x.split('/')[-1] for x in glob.glob('datasets/train/**')]
-
-# Load a COCO-pretrained YOLO11n model
-# model = YOLO("yolo11x-cls.pt")
-
-# Train the model on the COCO8 example dataset for 100 epochs
-# results = model.train(data="./datasets/", epochs=100, imgsz=640, batch=24, patience=10, name='nocontrol', classes= ['Compound_10',
-#  'Compound_1',
-#  'Compound_12',
-#  'Compound_11',
-#  'Compound_3',
-#  'Compound_8',
-#  'Compound_7',
-#  'Compound_14',
-#  'Compound_6',
-#  'Compound_17',
-#  'Compound_15',
-#  'Compound_13',
-#  'Compound_9',
-#  'Compound_5',
-#  'Compound_16',
-#  'Compound_2',
-#  'Compound_4'])
-
-#  import torch
-# torch.cuda.current_device()
