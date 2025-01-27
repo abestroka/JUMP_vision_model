b6a7638ff8d01b6841aa4385634dfb45e32411e6
abestroka <abestroka@gmail.com>
2025-01-26 23:36:33 -0600

    Update yolo_test.py

--- rad_pipeline/yolo_test.py
+++ rad_pipeline/yolo_test.py
@@ -14,7 +14,7 @@ import os
 import random
 
 # Path to the images directory
-base_path = '/eagle/FoundEpidem/astroka/yolo/rpe_rad_whole_1/images'
+base_path = '/eagle/FoundEpidem/astroka/yolo/rpe_rad_whole_9/images'
 
 # Number of files to keep in each subdirectory
 num_files_to_keep = 2000
