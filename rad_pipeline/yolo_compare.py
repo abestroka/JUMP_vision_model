7d2fe8e06d6436ab30bccd0e41c0dbb81e598d3d
abestroka <abestroka@gmail.com>
2025-02-28 16:28:34 -0600

    Update yolo_compare.py

--- rad_pipeline/yolo_compare.py
+++ rad_pipeline/yolo_compare.py
@@ -253,7 +253,7 @@ import random
 base_path = '/eagle/FoundEpidem/astroka/yolo/fibroblast_123/images'
 
 # Number of files to keep in each subdirectory
-num_files_to_keep = 5000
+num_files_to_keep = 2000
 
 # Find all subdirectories under the images directory
 subdirs = [d for d in glob.glob(os.path.join(base_path, '*')) if os.path.isdir(d)]
