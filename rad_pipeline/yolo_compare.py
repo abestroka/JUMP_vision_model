ae3176b64e0261b9de0a4f53fb00368659f9ce23
abestroka <abestroka@gmail.com>
2025-02-28 15:52:34 -0600

    Update yolo_compare.py

--- rad_pipeline/yolo_compare.py
+++ rad_pipeline/yolo_compare.py
@@ -250,7 +250,7 @@ import os
 import random
 
 # Path to the images directory
-base_path = '/eagle/FoundEpidem/astroka/yolo/all_cells_rad_1/images'
+base_path = '/eagle/FoundEpidem/astroka/yolo/fibroblast_123/images'
 
 # Number of files to keep in each subdirectory
 num_files_to_keep = 5000
@@ -277,7 +277,7 @@ for subdir in subdirs:
 
 
 # files = glob.glob('/eagle/FoundEpidem/astroka/yolo/rpe_rad_seg_2_vs_9/images/**/*p03.png')
-files = glob.glob('/eagle/FoundEpidem/astroka/yolo/all_cells_rad_1/images/**/*.png')
+files = glob.glob('/eagle/FoundEpidem/astroka/yolo/fibroblast_123/images/**/*.png')
 
 print("NUM FILES")
 print(len(files))
@@ -417,13 +417,13 @@ labels = [x.split('/')[-2] for x in files]
 train_dir = prepare_classification_data(
         np.array(files)[train_idx],
         np.array(labels)[train_idx],
-        '/eagle/FoundEpidem/astroka/yolo/all_cells_rad_1/data/dataset/train'
+        '/eagle/FoundEpidem/astroka/yolo/fibroblast_123/data/dataset/train'
     )
     
 val_dir = prepare_classification_data(
     np.array(files)[test_idx],
     np.array(labels)[test_idx],
-    '/eagle/FoundEpidem/astroka/yolo/all_cells_rad_1/data/dataset/val'
+    '/eagle/FoundEpidem/astroka/yolo/fibroblast_123/data/dataset/val'
 )
 
 # Define your class names
@@ -435,7 +435,7 @@ val_dir = prepare_classification_data(
 
 # class_names = ['week_two', 'week_eight']
 # class_names = ['0.001', '0.01', '0.1', '1.0', '2.0']
-class_names = ['huvec', 'fibroblast', 'rpe']
+class_names = ['week_one', 'week_two', 'week_three']
 
 
 
@@ -445,7 +445,7 @@ setup_classification_config(
     train_dir=train_dir,
     val_dir=val_dir,
     class_names=class_names,
-    config_path = '/eagle/FoundEpidem/astroka/yolo/all_cells_rad_1/data/datasets/config.yaml'
+    config_path = '/eagle/FoundEpidem/astroka/yolo/fibroblast_123/data/datasets/config.yaml'
     )
 
 
@@ -457,7 +457,7 @@ model = YOLO("yolo11x-cls.pt")
 
 
 # results = model.train(data="/eagle/FoundEpidem/astroka/yolo/rpe_rad_whole_1/data/dataset/", epochs=100, imgsz=640, batch=24, patience=10, name='rpe_rad_whole_1', classes= ['Compound_1', 'Compound_10', 'Compound_11', 'Compound_12','Compound_13', 'Compound_14', 'Compound_15', 'Compound_16', 'Compound_17', 'Compound_2', 'Compound_3', 'Compound_4', 'Compound_5', 'Compound_6', 'Compound_7', 'Compound_8', 'Compound_9', 'untreated'])
-results = model.train(data="/eagle/FoundEpidem/astroka/yolo/all_cells_rad_1/data/dataset/", epochs=100, imgsz=640, batch=24, patience=10, name='all_cells_rad_1', classes= ['huvec', 'fibroblast', 'rpe'])
+results = model.train(data="/eagle/FoundEpidem/astroka/yolo/fibroblast_123/data/dataset/", epochs=100, imgsz=640, batch=24, patience=10, name='fibroblast_123', classes= ['week_one', 'week_two', 'week_three'])
 
 
 
