0ca6dcd90ba59ef06c80a2e8a17385fe1116bb74
abestroka <abestroka@gmail.com>
2025-02-28 15:24:27 -0600

    Update yolo_compare.py

--- rad_pipeline/yolo_compare.py
+++ rad_pipeline/yolo_compare.py
@@ -253,7 +253,7 @@ import random
 base_path = '/eagle/FoundEpidem/astroka/yolo/all_cells_rad_1/images'
 
 # Number of files to keep in each subdirectory
-num_files_to_keep = 10000
+num_files_to_keep = 5000
 
 # Find all subdirectories under the images directory
 subdirs = [d for d in glob.glob(os.path.join(base_path, '*')) if os.path.isdir(d)]
