de4fd64b41afb380f01c6c331c3e1d4bd92b8aba
abestroka <abestroka@gmail.com>
2025-03-17 11:23:09 -0500

    chart for 1 vs 12 week 1

--- rad_pipeline/chart_averages.py
+++ rad_pipeline/chart_averages.py
@@ -139,12 +139,12 @@ def extract_intensity_averages(repo_path):
                 try:
                     df = pd.read_csv(file_path)
                     if 'Intensity_MedianIntensity_RescaleER' in df.columns:
-                        avg_intensity = df['Intensity_MedianIntensity_RescaleER'].mean()
+                        avg_intensity = df['Intensity_MedianIntensity_RescaleRNA'].mean()
                         averages.append(avg_intensity)
                         labels.append(file.split('.')[0])  # Extract filename without extension
                         # print(f"Processed: {file}, Average Intensity: {avg_intensity}")
                     else:
-                        print(f"Column 'Intensity_MedianIntensity_RescaleER' not found in {file}")
+                        print(f"Column 'Intensity_MedianIntensity_RescaleRNA' not found in {file}")
                 except Exception as e:
                     print(f"Error processing {file}: {e}")
     
@@ -191,30 +191,30 @@ def plot_comparison(repo1_averages, repo2_averages, labels, save_path):
     
     plt.figure(figsize=(12, 6))
     plt.plot(x, repo1_averages, marker='o', linestyle='-', label='DMSO')
-    plt.plot(x, repo2_averages, marker='s', linestyle='-', label='Compound 4')
+    plt.plot(x, repo2_averages, marker='s', linestyle='-', label='Compound 12')
 
     avg1 = np.mean(repo1_averages)
     avg2 = np.mean(repo2_averages)
     print("DMSO AVG:", avg1)
     print("Compound AVG:", avg2)
     plt.axhline(y=avg1, color='r', linestyle='--', label='DMSO Average')
-    plt.axhline(y=avg2, color='b', linestyle='--', label='Compound 4 Average')
+    plt.axhline(y=avg2, color='b', linestyle='--', label='Compound 12 Average')
 
     
     plt.xlabel("Well Location")
     plt.ylabel("Average Intensity")
-    plt.title("DMSO vs Compound 4 Average Median Ch6 Intensity Per Field Week 9")
+    plt.title("DMSO vs Compound 12 Average Median Ch4 Intensity Per Field Week 1")
     plt.legend()
     plt.tight_layout()
     plt.savefig(save_path)
     plt.close()
     # print(f"Plot saved to {save_path}")
 
-repo1_averages, repo1_labels = extract_intensity_averages("/eagle/projects/FoundEpidem/astroka/rpe/week_nine/ind_channels_seg_control/ch6/csv_files_Compound_1")
-repo2_averages, repo2_labels = extract_intensity_averages("/eagle/projects/FoundEpidem/astroka/rpe/week_nine/ind_channels_seg_control/ch6/csv_files_Compound_4")
+repo1_averages, repo1_labels = extract_intensity_averages("/eagle/projects/FoundEpidem/astroka/rpe/week_one/ind_channels_seg_control/ch4/csv_files_Compound_1")
+repo2_averages, repo2_labels = extract_intensity_averages("/eagle/projects/FoundEpidem/astroka/rpe/week_one/ind_channels_seg_control/ch4/csv_files_Compound_12")
 
 if repo1_labels == repo2_labels:
-    plot_comparison(repo1_averages, repo2_averages, repo1_labels, "/eagle/projects/FoundEpidem/astroka/rpe/week_one/week_9_compounds_1_vs_4_ch6_line.png")
+    plot_comparison(repo1_averages, repo2_averages, repo1_labels, "/eagle/projects/FoundEpidem/astroka/rpe/week_one/week_1_compounds_1_vs_12_ch4_line.png")
 else:
     print("Labels do not match between repositories.")
-    plot_comparison(repo1_averages, repo2_averages, repo1_labels, "/eagle/projects/FoundEpidem/astroka/rpe/week_one/week_9_compounds_1_vs_4_ch6_line.png")
+    plot_comparison(repo1_averages, repo2_averages, repo1_labels, "/eagle/projects/FoundEpidem/astroka/rpe/week_one/week_1_compounds_1_vs_12_ch4_line.png")
