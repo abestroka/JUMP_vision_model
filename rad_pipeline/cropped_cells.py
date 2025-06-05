f0d281f0c9ac062312ceb6f49874d514c5cda608
abestroka <abestroka@gmail.com>
2025-06-04 22:51:16 -0500

    no rescale week 1

--- rad_pipeline/cropped_cells.py
+++ rad_pipeline/cropped_cells.py
@@ -237,7 +237,7 @@ if __name__ == '__main__':
         '--cellprofiler_pipeline',
         help='The cell profiler pipeline to use',
         default=Path(
-            '/lus/eagle/projects/FoundEpidem/astroka/cropped_cells.cppipe',
+            '/lus/eagle/projects/FoundEpidem/astroka/cropped_cells_no_rescale.cppipe',
         ),
         type=Path,
     )
