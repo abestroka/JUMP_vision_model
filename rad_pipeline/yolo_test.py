41209fee894b62d3dafea892e777add00e7a105c
abestroka <abestroka@gmail.com>
2025-06-11 15:45:17 -0500

    Update yolo_test.py

--- rad_pipeline/yolo_test.py
+++ rad_pipeline/yolo_test.py
@@ -17,7 +17,7 @@ import random
 base_path = '/eagle/FoundEpidem/astroka/yolo/huvec_control_seg_2/images/'
 
 # Number of files to keep in each subdirectory
-num_files_to_keep = 2000
+num_files_to_keep = 500
 
 # Find all subdirectories under the images directory
 subdirs = [d for d in glob.glob(os.path.join(base_path, '*')) if os.path.isdir(d)]
