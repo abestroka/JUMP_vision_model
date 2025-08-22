ca6a4dfde1be742f709a896f51c3fb9178243cf3
abestroka <abestroka@gmail.com>
2025-08-22 11:34:31 -0500

    Update run_cell_outline.sh

--- rad_pipeline/run_cell_outline.sh
+++ rad_pipeline/run_cell_outline.sh
@@ -37,7 +37,7 @@ SECONDS=0
 
 
 
-plate="Control"
+plate="Control2"
 # plate="HUVEC_Control"
 # week='week_one'
 images="/eagle/FoundEpidem/astroka/rpe_h2a/week_eight/20250701_RPE-1_H2A_Week8/20250701_20x_ANL_CellPainting_RPE-1_Control_1__2025-07-01T19_21_41-Measurement1/Images"
