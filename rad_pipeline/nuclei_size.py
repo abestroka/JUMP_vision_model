9270c29a6eb50a2283e05b71ed7fa4bd305da96d
abestroka <abestroka@gmail.com>
2025-09-11 15:50:06 -0500

    update

--- rad_pipeline/nuclei_size.py
+++ rad_pipeline/nuclei_size.py
@@ -2,14 +2,79 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from statistics import mean
 
 repo_path = "/eagle/projects/FoundEpidem/astroka/ten_week/"
 
 nuclei_size_avgs = []
-doses = []
+doses = ["0.001", "0.01", "0.1", "1.0", "2.0"]
 type = "huvec_rad"
+results = {dose: [] for dose in doses}
 
 week_folders = [f for f in os.listdir(repo_path) if f.startswith("week_")]
 
 for week in week_folders:
-    week_path = os.path.join(repo_path, week, "results", type)
+    week_path = os.path.join(repo_path, week, "results", "huvec_rad")
+    if not os.path.isdir(week_path):
+        continue
+
+    week_vals = []
+
+    for dose in doses:
+        dose_path = os.path.join(week_path, dose)
+        if not os.path.isdir(dose_path):
+            results[dose].append(np.nan)
+            continue
+        
+        values = []
+        for file in os.listdir(dose_path):
+            if file.endswith("nuclei.csv"):
+                df = pd.read_csv(os.path.join(dose_path, file))
+                col_data = df["AreaShape_Area"].tolist()
+        
+            values.append(mean(col_data))
+        
+        week_vals.append(values)
+    nuclei_size_avgs.append(week_vals)
+
+for week in week_folders:
+    week_path = os.path.join(repo_path, week, "results", "huvec_control")
+    if not os.path.isdir(week_path):
+        continue
+
+    week_vals = []
+
+    # for dose in doses:
+    #     dose_path = os.path.join(week_path, dose)
+    #     if not os.path.isdir(dose_path):
+    #         results[dose].append(np.nan)
+    #         continue
+
+    dose_path = os.path.join(week_path, "untreated")
+        
+    values = []
+    for file in os.listdir(dose_path):
+        if file.endswith("nuclei.csv"):
+            df = pd.read_csv(os.path.join(dose_path, file))
+            col_data = df["AreaShape_Area"].tolist()
+    
+        values.append(mean(col_data))
+        
+    week_vals.append(values)
+
+nuclei_size_avgs.append(week_vals)
+
+
+doses.append("untreated")
+
+plt.boxplot(nuclei_size_avgs, labels=doses)
+
+plt.ylabel("Nuclei Size")
+plt.xlabel("Dose")
+plt.title("Huvec Nuclei Size")
+
+repo_path = "/eagle/projects/FoundEpidem/astroka/ten_week/"
+
+png_path = os.path.join(repo_path, "average_nuclei_size.png")
+plt.savefig(png_path, dpi=300)
+plt.close()
