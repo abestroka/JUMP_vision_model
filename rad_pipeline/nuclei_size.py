6b0e8fc1dab17490ed0e47356085d10ba7d9ecb5
abestroka <abestroka@gmail.com>
2025-09-15 00:55:30 -0500

    Update nuclei_size.py

--- rad_pipeline/nuclei_size.py
+++ rad_pipeline/nuclei_size.py
@@ -37,22 +37,10 @@ for week in week_folders:
                     values.append(mean(col_data))
         
         week_vals.append(values)
-    nuclei_size_avgs.append(week_vals)
 
-for week in week_folders:
-    week_path = os.path.join(repo_path, week, "sheets", "rpe_control")
-    if not os.path.isdir(week_path):
-        continue
+################
 
-    week_vals = []
-
-    # for dose in doses:
-    #     dose_path = os.path.join(week_path, dose)
-    #     if not os.path.isdir(dose_path):
-    #         results[dose].append(np.nan)
-    #         continue
-
-    dose_path = os.path.join(week_path, "untreated")
+    dose_path = os.path.join(repo_path, week, "sheets", "rpe_control", "untreated")
         
     values = []
     for file in os.listdir(dose_path):
@@ -62,10 +50,31 @@ for week in week_folders:
             # print("UNTREATED", col_data)
             if col_data != []:
                 values.append(mean(col_data))
-        
     week_vals.append(values)
 
-nuclei_size_avgs.append(week_vals)
+    nuclei_size_avgs.append(week_vals)
+
+# for week in week_folders:
+#     week_path = os.path.join(repo_path, week, "sheets", "rpe_control")
+#     if not os.path.isdir(week_path):
+#         continue
+
+#     week_vals = []
+
+#     dose_path = os.path.join(week_path, "untreated")
+        
+#     values = []
+#     for file in os.listdir(dose_path):
+#         if file.endswith("nuclei.csv"):
+#             df = pd.read_csv(os.path.join(dose_path, file))
+#             col_data = df["AreaShape_Area"].tolist()
+#             # print("UNTREATED", col_data)
+#             if col_data != []:
+#                 values.append(mean(col_data))
+        
+#     week_vals.append(values)
+
+# nuclei_size_avgs.append(week_vals)
 
 
 doses.append("untreated")
