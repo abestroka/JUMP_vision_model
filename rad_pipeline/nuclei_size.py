8d630a10da3f7d2a1fa2fcbd1e92a6f66ce16c84
abestroka <abestroka@gmail.com>
2025-09-15 01:18:56 -0500

    Update nuclei_size.py

--- rad_pipeline/nuclei_size.py
+++ rad_pipeline/nuclei_size.py
@@ -84,16 +84,18 @@ doses.append("untreated")
 
 # colors = plt.cm.tab10.colors[:6]
 
-data = np.array(nuclei_size_avgs)
+# data = np.array(nuclei_size_avgs)
 
 
-colors = plt.cm.tab10.colors[:data.shape[1]]  # 6 distinct colors
+# colors = plt.cm.tab10.colors[:data.shape[1]]  # 6 distinct colors
 
-fig, axes = plt.subplots(1, data.shape[1], figsize=(20, 6), sharey=True)
+colors = plt.cm.tab10.colors[:len(nuclei_size_avgs[0])]
 
-for j in range(data.shape[1]):  # 6 groups
-    # Collect data for this group: list of 9 arrays, each of length 180
-    group_data = [data[i, j, :] for i in range(data.shape[0])]
+fig, axes = plt.subplots(1, len(nuclei_size_avgs[0]), figsize=(20, 6), sharey=True)
+
+for j in range(len(nuclei_size_avgs[0])):  # 6 groups
+    # Collect data for this group: list of 9 arrays, each length 180
+    group_data = [nuclei_size_avgs[i][j] for i in range(len(nuclei_size_avgs))]
     
     bp = axes[j].boxplot(group_data, patch_artist=True, widths=0.6)
     
@@ -102,13 +104,13 @@ for j in range(data.shape[1]):  # 6 groups
         patch.set_facecolor(colors[j])
         patch.set_alpha(0.6)
     
-    axes[j].set_xticks(range(1, data.shape[0] + 1))
+    axes[j].set_xticks(range(1, len(nuclei_size_avgs) + 1))
     axes[j].set_xticklabels(doses, rotation=45, ha="right")
     axes[j].set_title(f"Group {j+1}")
 
 axes[0].set_ylabel("Values")
 plt.tight_layout()
-# plt.show()
+plt.show()
 
 repo_path = "/eagle/projects/FoundEpidem/astroka/ten_week/"
 
