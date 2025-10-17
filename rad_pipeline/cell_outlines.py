3f2bdd4089aff37443faccef4d4210f30e4e5e61
abestroka <abestroka@gmail.com>
2025-10-17 12:20:06 -0500

    huvec 1 rad seg

--- rad_pipeline/cell_outlines.py
+++ rad_pipeline/cell_outlines.py
@@ -282,7 +282,7 @@ if __name__ == '__main__':
         '--cellprofiler_pipeline',
         help='The cell profiler pipeline to use',
         default=Path(
-            '/lus/eagle/projects/FoundEpidem/astroka/cropped_cells_rescale.cppipe',
+            '/lus/eagle/projects/FoundEpidem/astroka/cropped_cells.cppipe',
         ),
         type=Path,
     )
