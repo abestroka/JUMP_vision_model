86e478ed5e4a702236a697014c7c6378fb70f248
abestroka <abestroka@gmail.com>
2025-10-27 11:12:51 -0500

    Update yolo_compare.py

--- rad_pipeline/yolo_compare.py
+++ rad_pipeline/yolo_compare.py
@@ -203,7 +203,7 @@ val_dir = prepare_classification_data(
 
 # class_names = ['week_two', 'week_eight']
 # class_names = ['0.001', '0.01', '0.1', '1.0', '2.0']
-class_names = ['week_1', 'week_2', 'week_3']
+class_names = ['week_1', 'week_2', 'week_3', 'week_4']
 
 
 
@@ -225,7 +225,7 @@ model = YOLO("yolo11x-cls.pt")
 
 
 # results = model.train(data="/eagle/FoundEpidem/astroka/yolo/rpe_rad_whole_1/data/dataset/", epochs=100, imgsz=640, batch=24, patience=10, name='rpe_rad_whole_1', classes= ['Compound_1', 'Compound_10', 'Compound_11', 'Compound_12','Compound_13', 'Compound_14', 'Compound_15', 'Compound_16', 'Compound_17', 'Compound_2', 'Compound_3', 'Compound_4', 'Compound_5', 'Compound_6', 'Compound_7', 'Compound_8', 'Compound_9', 'untreated'])
-results = model.train(data="/eagle/FoundEpidem/astroka/yolo/fib_123_untreated/data/dataset/", epochs=100, imgsz=640, batch=24, patience=10, name='fib_123_untreated', classes= ['week_1', 'week_2', 'week_3'])
+results = model.train(data="/eagle/FoundEpidem/astroka/yolo/fib_123_untreated/data/dataset/", epochs=100, imgsz=640, batch=24, patience=10, name='fib_123_untreated', classes= ['week_1', 'week_2', 'week_3', 'week_4'])
 
 
 
