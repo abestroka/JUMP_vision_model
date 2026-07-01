702a12b5828c7d8358463a7dca97d628ee9407a8
abestroka <abestroka@gmail.com>
2026-07-01 11:41:57 -0500

    Update run_cell_outline.sh

--- rad_pipeline/run_cell_outline.sh
+++ rad_pipeline/run_cell_outline.sh
@@ -51,9 +51,9 @@ SECONDS=0
 # # python ~/workspace/JUMP_vision_model/rad_pipeline/cropped_cells.py -i $images -p $plate -t $treatment_file -o /eagle/projects/FoundEpidem/astroka/ten_week/week_one_corrected/results/huvec_control_seg --tmp_dir /dev/shm --num_workers 32
 
 
-plate="Plate3"
-images="/grand/FRAME-IDP/astroka/exp_05_26/week_four/20260716_ANL_Week4/20260617_20x_ANL_CellPainting_RPE-1_Plate3_1__2026-06-16T18_41_35-Measurement1/Images"
+plate="Control"
+images="/grand/FRAME-IDP/astroka/exp_05_26/week_four/20260716_ANL_Week4/20260617_20x_ANL_CellPainting_RPE-1_Control_1__2026-06-16T21_30_26-Measurement1/Images"
 treatment_file="/home/astroka/workspace/JUMP_vision_model/rad_pipeline/rpe1_ko_wt.xlsx"
-python ~/workspace/JUMP_vision_model/rad_pipeline/cell_outlines.py -i $images -p $plate -t $treatment_file -o /grand/projects/FRAME-IDP/astroka/exp_05_26/week_four/results/Plate3/ --tmp_dir /dev/shm --num_workers 16
+python ~/workspace/JUMP_vision_model/rad_pipeline/cell_outlines.py -i $images -p $plate -t $treatment_file -o /grand/projects/FRAME-IDP/astroka/exp_05_26/week_four/results/Control/ --tmp_dir /dev/shm --num_workers 16
 
 # python ~/workspace/JUMP_vision_model/rad_pipeline/tiffs_preprocess.py
