dd587d6a5987c0fc9b62dc47c2b346feae0ebe62
abestroka <abestroka@gmail.com>
2026-07-13 10:53:21 -0500

    Update run_cell_outline.sh

--- rad_pipeline/run_cell_outline.sh
+++ rad_pipeline/run_cell_outline.sh
@@ -52,8 +52,8 @@ SECONDS=0
 
 
 plate="Control"
-images="/grand/FRAME-IDP/astroka/exp_05_26/week_four/20260716_ANL_Week4/20260617_20x_ANL_CellPainting_RPE-1_Control_1__2026-06-16T21_30_26-Measurement1/Images"
+images="/grand/FRAME-IDP/astroka/exp_05_26/week_five/20260705_ANL_Week5/20260701_ANL_Week5_PlateCtl_1__2026-07-01T17_00_28-Measurement1/Images"
 treatment_file="/home/astroka/workspace/JUMP_vision_model/rad_pipeline/rpe1_ko_wt.xlsx"
-python ~/workspace/JUMP_vision_model/rad_pipeline/cell_outlines.py -i $images -p $plate -t $treatment_file -o /grand/projects/FRAME-IDP/astroka/exp_05_26/week_four/results/Control/ --tmp_dir /dev/shm --num_workers 16
+python ~/workspace/JUMP_vision_model/rad_pipeline/cell_outlines.py -i $images -p $plate -t $treatment_file -o /grand/projects/FRAME-IDP/astroka/exp_05_26/week_five/results/Control/ --tmp_dir /dev/shm --num_workers 16
 
 # python ~/workspace/JUMP_vision_model/rad_pipeline/tiffs_preprocess.py
