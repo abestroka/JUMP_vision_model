fc75c65186b5a6567427935eacc899cb8a87787d
abestroka <abestroka@gmail.com>
2026-07-27 11:26:49 -0500

    Update run_cell_outline.sh

--- rad_pipeline/run_cell_outline.sh
+++ rad_pipeline/run_cell_outline.sh
@@ -38,7 +38,7 @@ SECONDS=0
 
 
 plate="Plate8"
-images="/grand/FRAME-IDP/astroka/exp_05_26/week_three/20260610_ANL_Week3/20260609_20x_ANL_CellPainting_RPE-1_Plate8_1__2026-06-09T18_45_29-Measurement1/Images"
-treatment_file="/home/astroka/workspace/JUMP_vision_model/rad_pipeline/rpe1_ko_wt_001.xlsx"
-python ~/workspace/JUMP_vision_model/rad_pipeline/cell_outlines.py -i $images -p $plate -t $treatment_file -o /grand/projects/FRAME-IDP/astroka/exp_05_26/week_three/results/Plate8/ --tmp_dir /dev/shm --num_workers 16
+images="/grand/FRAME-IDP/astroka/exp_05_26/week_four/20260716_ANL_Week4/20260617_20x_ANL_CellPainting_RPE-1_Plate8_1__2026-06-16T20_00_30-Measurement1/Images"
+treatment_file="/home/astroka/workspace/JUMP_vision_model/rad_pipeline/rpe1_ko_wt.xlsx"
+python ~/workspace/JUMP_vision_model/rad_pipeline/cell_outlines.py -i $images -p $plate -t $treatment_file -o /grand/projects/FRAME-IDP/astroka/exp_05_26/week_four/results/Plate8/ --tmp_dir /dev/shm --num_workers 16
 
